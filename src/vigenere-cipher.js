2b561bdea58a8ed208231592b8f92f4c2f8da19b
ldemyanov <leonidemyanov@gmail.com>
2022-10-15 01:52:19 +0800

    feat: implement solve to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,96 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(flag = true) {
+    this.isReverse = flag
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(string, keyword) {
+    if (typeof string !== "string" || typeof keyword !== "string") {
+      throw new Error('Incorrect arguments!')
+    }
+
+    const latinAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    let lenString = string.length
+
+    let preparedString = ""
+    string.toUpperCase().split("").forEach(s => latinAlphabet.includes(s) && (preparedString += s))
+    let lenPreparedString = preparedString.length
+
+
+    let lenKeyword = keyword.length
+    let tableKeyword = keyword
+
+    if (lenKeyword < lenPreparedString) {
+      let count = Math.ceil(lenPreparedString / lenKeyword)
+      tableKeyword = keyword.repeat(count).substring(0, lenPreparedString)
+    } else if (lenKeyword > lenPreparedString) {
+      tableKeyword = keyword.substring(0, lenPreparedString)
+    }
+    tableKeyword = tableKeyword.toUpperCase()
+
+    let result = ""
+    
+    let move = 0
+    for (let i = 0; i < lenString; i++) {
+      let s = string[i]
+      let posS = latinAlphabet.indexOf(s.toUpperCase())
+      
+      if (posS !== -1) {
+        let posKeyS = latinAlphabet.indexOf(tableKeyword[i+move])
+        result += latinAlphabet[ (posS + posKeyS) % latinAlphabet.length ]
+      } else {
+        result += s
+        move--
+      }
+    }
+
+    return this.isReverse ? result : result.split("").reverse().join("")
+    
+}
+  decrypt(string, keyword) {
+    if (typeof string !== "string" || typeof keyword !== "string") {
+      throw new Error('Incorrect arguments!')
+    }
+
+    const latinAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    let lenString = string.length
+
+    let preparedString = ""
+    string.toUpperCase().split("").forEach(s => latinAlphabet.includes(s) && (preparedString += s))
+    let lenPreparedString = preparedString.length
+
+
+    let lenKeyword = keyword.length
+    let tableKeyword = keyword
+
+    if (lenKeyword < lenPreparedString) {
+      let count = Math.ceil(lenPreparedString / lenKeyword)
+      tableKeyword = keyword.repeat(count).substring(0, lenPreparedString)
+    } else if (lenKeyword > lenPreparedString) {
+      tableKeyword = keyword.substring(0, lenPreparedString)
+    }
+    tableKeyword = tableKeyword.toUpperCase()
+
+    let result = ""
+    
+    let move = 0
+    for (let i = 0; i < lenString; i++) {
+      let s = string[i]
+      let posS = latinAlphabet.indexOf(s.toUpperCase())
+      
+      if (posS !== -1) {
+        let posKeyS = latinAlphabet.indexOf(tableKeyword[i+move])
+        result += latinAlphabet[ (posS + latinAlphabet.length - posKeyS) % latinAlphabet.length ]
+      } else {
+        result += s
+        move--
+      }
+    }
+    
+    return this.isReverse ? result : result.split("").reverse().join("")
   }
 }
 
